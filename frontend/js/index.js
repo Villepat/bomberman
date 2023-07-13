6317776d8956c98a32d10b36c9c97ab9ffe516f0
Rihizz <santeri.riihela@gmail.com>
2023-07-13 12:47:14 +0300

    websocket added and yes

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,11 @@
 import buildBaseGrid from "./game_board/build_base.js";
 
+const ws = new WebSocket("ws://localhost:80/ws");
+
+ws.addEventListener("open", function (event) {
+  console.log("WebSocket connection established");
+});
+
+console.log("Hello from index.js");
+
 buildBaseGrid(19);
