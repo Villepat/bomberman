caa1e72d171125cfb1c26ddd550298e593500d01
Rihizz <santeri.riihela@gmail.com>
2023-07-21 10:24:39 +0300

    bombs fixed :o?

--- backend/server/ws.go
+++ backend/server/ws.go
@@ -203,13 +203,13 @@ func MovePlayer(gameGrid [19][19]int, playerID int, direction string) {
 	//Get the player from the map
 	player := game_functions.Players[playerID]
 
-	speed := player.Speed
-	lastMove := player.LastMove
-	//call isAllowedToMove to check if the player is allowed to move
-	if !IsAllowedToMove(speed, lastMove) {
-		fmt.Println("Player is not allowed to move yet")
-		return
-	}
+	// speed := player.Speed
+	// lastMove := player.LastMove
+	// //call isAllowedToMove to check if the player is allowed to move
+	// if !IsAllowedToMove(speed, lastMove) {
+	// 	fmt.Println("Player is not allowed to move yet")
+	// 	return
+	// }
 
 	//Change the player's direction
 	player.Direction = direction
@@ -313,7 +313,7 @@ func PlaceBomb(gameGrid *[19][19]int, playerID int) {
 		return
 	}
 	// Check if the player is on a bomb
-	if gameGrid[player.GridPosition[0]][player.GridPosition[1]] == 69 {
+	if gameGrid[player.GridPosition[1]][player.GridPosition[0]] == 69 {
 		log.Println("Player is on a bomb")
 		defer gridMutex.Unlock()
 		return
@@ -323,7 +323,7 @@ func PlaceBomb(gameGrid *[19][19]int, playerID int) {
 	log.Println("Bomb range: ", bombRange)
 
 	// Place a bomb on the game board
-	gameGrid[player.GridPosition[0]][player.GridPosition[1]] = 69
+	gameGrid[player.GridPosition[1]][player.GridPosition[0]] = 69
 	log.Println("Bomb placed at: ", player.GridPosition)
 	// Update the player in the map
 	player.Bombs--
@@ -386,8 +386,10 @@ func PlaceBomb(gameGrid *[19][19]int, playerID int) {
 func HandleExplosion(gameGrid *[19][19]int, x int, y int, bombRange int) {
 	gridMutex.Lock()
 	log.Println("---------------------BOOOOOOOOOOOOOOOOOOOOM---------------------")
+	printBoard(gameGrid)
+	log.Println("Bomb exploded at: ", x, y)
 	// Clean the bomb from gameGrid
-	gameGrid[x][y] = 0
+	gameGrid[y][x] = 0
 
 	// The explosion's affected area is the bomb range in each direction.
 	var affectedCells [][]int
@@ -405,7 +407,7 @@ func HandleExplosion(gameGrid *[19][19]int, x int, y int, bombRange int) {
 	gridMutex.Unlock()
 
 	// Log the state of game grid after explosion
-	log.Println("Game grid after explosion: ", gameGrid)
+	printBoard(gameGrid)
 	log.Println("Affected cells: ", affectedCells)
 
 	explosionMsg := Msg{
@@ -425,5 +427,17 @@ func HandleExplosion(gameGrid *[19][19]int, x int, y int, bombRange int) {
 		if err != nil {
 			log.Println(err)
 		}
+	}
+}
+
+func printBoard(board *[19][19]int) {
+	// Start printing from the last row to the first
+	for i := len(board) - 1; i >= 0; i-- {
+		row := board[i]
+		for _, cell := range row {
+			// Each cell is printed with a fixed width of 3 for better alignment
+			fmt.Printf("%3d", cell)
+		}
+		fmt.Println() // Move to the next line after printing each row
 	}
 }
